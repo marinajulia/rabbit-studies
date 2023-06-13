603b1bda2d47515fda4b5f2c2f58e9c398a59565
marinajulia <marinajulia707@gmail.com>
2023-06-13 07:48:59 -0300

    Creation of the first message

---
+++ Publisher/Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publischer 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saudacao_1",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Bem vindo ao RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "saudacao_1",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine($"[x] Enviada: {message}");
+
+            }
+        }
+    }
+}
